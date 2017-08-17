78fa70ab07a514b723c8ecfe5d523cc4d62ac2e7
Kohei Yoshino <kohei.yoshino@gmail.com>
2017-08-16 23:21:17 -0400

    Add license header

--- extension/content.js
+++ extension/content.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /**
  * Parse a message from the frontend script and execute a specified command.
  * @param {Object.<String,*>} message - A message object from frontend.
